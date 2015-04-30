251e6284b9735d000ba668f95986292766230596
ezeddin <e.alhakim@gmail.com>
2015-04-30 14:38:22 +0200

    V2

--- s4.hs
+++ s4.hs
@@ -12,9 +12,52 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             | Command String LispVal
+
+
+main :: IO ()
+main = do
+         args <- getContents
+         putStrLn (readExpr (args))
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" (map C.toLower input) of
+    Left err -> "No match: " ++ show err
+    Right _ -> "Found value"
+
+parseExpr :: Parser LispVal
+parseExpr = parseCommand
+         -- <|> parseAtom
+         -- <|> parseString
+         -- <|> parseNumber
+         --- <|> parseQuoted
+         -- <|> do char '('
+         --    x <- try parseList <|> parseDottedList
+         --    char ')'
+         --    return x
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseCommand :: Parser LispVal
+parseCommand = do
+                cmd <- (string "forw") <|> (string "back")
+                spaces
+                nmb <- parseNumber
+                return $ Command cmd nmb
+
+
+
+
+
+
+
+
+
+
 
 parseString :: Parser LispVal
 parseString = do
@@ -33,44 +76,10 @@ parseAtom = do
                          "#f" -> Bool False
                          _    -> Atom atom
 
-parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
-
-parseList :: Parser LispVal
-parseList = liftM List $ sepBy parseExpr spaces
-
-parseCommand :: Parser LispVal
-parseCommand = do
-                cmd <- (string "forw") <|> (string "back")
-                return $ Atom cmd
-
-parseDottedList :: Parser LispVal
-parseDottedList = do
-    head <- endBy parseExpr spaces
-    tail <- char '.' >> spaces >> parseExpr
-    return $ DottedList head tail
-
 parseQuoted :: Parser LispVal
 parseQuoted = do
     char '\''
     x <- parseExpr
     return $ List [Atom "quote", x]
-readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" (map C.toLower input) of
-    Left err -> "No match: " ++ show err
-    Right _ -> "Found value"
 
-parseExpr :: Parser LispVal
-parseExpr = parseCommand
-         -- <|> parseAtom
-         <|> parseString
-         <|> parseNumber
-         <|> parseQuoted
-         <|> do char '('
-                x <- try parseList <|> parseDottedList
-                char ')'
-                return x
-main :: IO ()
-main = do
-         args <- getArgs
-         putStrLn (readExpr (args !! 0))
+
