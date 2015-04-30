f04325b7ee2cd07d4ddf59aec7aa1388c8043348
ezeddin <e.alhakim@gmail.com>
2015-04-30 14:55:02 +0200

    asdsa

--- s4.hs
+++ s4.hs
@@ -26,7 +26,7 @@ readExpr input = case parse parseExpr "lisp" (map C.toLower input) of
     Right _ -> "Found value"
 
 parseExpr :: Parser LispVal
-parseExpr = parseCommand
+parseExpr = parseCommand <|> parseExpr
          -- <|> parseAtom
          -- <|> parseString
          -- <|> parseNumber
