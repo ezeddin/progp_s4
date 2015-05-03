fa4371597b0f2ff9d0f4e6bf694e6184daeda982
ezeddin <e.alhakim@gmail.com>
2015-05-03 21:30:23 +0200

    fix new line

--- s4.hs
+++ s4.hs
@@ -11,93 +11,94 @@ showCommand :: LispVal -> String
 showCommand (Command str nmb) = str ++ (show nmb)
 showCommand (Number int) = show int
 showCommand EOF = "EOF"
-showCommand _ = "hej"
+showCommand _ = "OTHER"
+
+-- data Dot = Dot Char
+-- data Number = Number Integer
+-- data Hex = Hex String
+-- 
+-- data Command = Command
+--              | Movement Number
+--              | Rotation Number
+--              | Color Hex
+--              | WritingMode
+--              | EOF
+-- 
+-- data Repetition = Repetition Number [Command]
+-- 
+-- data Leonardo = Command | Repetition | Dot | Number | Hex
+
 data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
              | EOF
+             | Dot Char
              | NewLine Char
              | Number Integer
              | String String
              | Bool Bool
              | Command String LispVal
 
-
-
-
 main :: IO ()
 main = do
          args <- getContents
          putStrLn (readExpr (args))
 
-readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" (map C.toLower input) of
+readExpr input = case parse (spaces0 >> parseExpr) "Leonardo" (map C.toLower input) of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value " ++ (show val)
+    Right val -> "Found value" ++ show val
+
 
 parseExpr :: Parser LispVal
-parseExpr = (parseCommand
-            <|> parseNewLine) >>
-            (parseEOF <|> parseExpr)
-         -- <|> parseAtom
-         -- <|> parseString
-         -- <|> parseNumber
-         --- <|> parseQuoted
-         -- <|> do char '('
-         --    x <- try parseList <|> parseDottedList
-         --    char ')'
-         --    return x
+parseExpr = parseCommand     
+            >>  spaces0 >> (parseEOF <|> parseExpr)
 
-spaces :: Parser ()
-spaces = skipMany1 space
+spaces1 :: Parser ()
+spaces1 = skipMany1 (space <|> newline)
+
+spaces0 :: Parser ()
+spaces0 = skipMany (space <|> newline)
 
 parseEOF :: Parser LispVal
 parseEOF = do
             eof
             return EOF
-parseNewLine :: Parser LispVal
-parseNewLine =  do
-                chr <- (char '\n')
-                return $ NewLine chr
+
+parseDot :: Parser LispVal
+parseDot =  do
+                chr <- (char '.')
+                return $ Dot chr
+
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
 
 parseCommand :: Parser LispVal
 parseCommand = do
                 cmd <- (string "forw") <|> (string "back")
-                spaces
+                spaces1
                 nmb <- parseNumber
+                spaces0
+                dot <- parseDot
                 return $ Command cmd nmb
 
 
-
-
-
-
-
-
-
-
-
-parseString :: Parser LispVal
-parseString = do
-                char '"'
-                x <- many (noneOf "\"")
-                char '"'
-                return $ String x
-
-parseAtom :: Parser LispVal
-parseAtom = do
-              first <- letter <|> symbol
-              rest <- many (letter <|> digit <|> symbol)
-              let atom = first:rest
-              return $ case atom of
-                         "#t" -> Bool True
-                         "#f" -> Bool False
-                         _    -> Atom atom
-
-parseQuoted :: Parser LispVal
-parseQuoted = do
-    char '\''
-    x <- parseExpr
-    return $ List [Atom "quote", x]
+-- parseString :: Parser LispVal
+-- parseString = do
+--                 char '"'
+--                 x <- many (noneOf "\"")
+--                 char '"'
+--                 return $ String x
+-- 
+-- parseAtom :: Parser LispVal
+-- parseAtom = do
+--               first <- letter <|> symbol
+--               rest <- many (letter <|> digit <|> symbol)
+--               let atom = first:rest
+--               return $ case atom of
+--                          "#t" -> Bool True
+--                          "#f" -> Bool False
+--                          _    -> Atom atom
+-- 
+-- parseQuoted :: Parser LispVal
+-- parseQuoted = do
+--     char '\''
+--     x <- parseExpr
+--     return $ List [Atom "quote", x]
